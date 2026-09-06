3ce229a18c66ad3a5bd1fedfe059687bf82feb32
Daniel Lopes <danielsoutolopes@gmail.com>
2026-09-06 11:18:31 -0300

    fix build

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 const globalForPrisma = global as unknown as { prismaClientV2: PrismaClient };
 
-const connectionString = process.env.DATABASE_URL || "postgresql://postgres:placeholder@localhost:5432/postgres";
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
 export const prisma =
   globalForPrisma.prismaClientV2 ||
-  new PrismaClient({ adapter });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prismaClientV2 = prisma;
